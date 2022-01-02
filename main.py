94c64b229373857f551f35c7662b6741aa0f318a
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-01-02 12:00:37 -0800

    Add untested changes of first version

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 from argparse import ArgumentParser
 import json
+import re
+from re import Match
+
+
+ROMAN_VALUES = [
+    (1000, 'M'),
+    (900, 'CM'),
+    (500, 'D'),
+    (400, 'CD'),
+    (100, 'C'),
+    (90, 'XC'),
+    (50, 'L'),
+    (40, 'XL'),
+    (10, 'X'),
+    (9, 'IX'),
+    (5, 'V'),
+    (4, 'IV'),
+    (1, 'I')
+]
+
+COLOR_VALUES = {
+    '0': 'black',
+    '1': 'dark_blue',
+    '2': 'dark_green',
+    '3': 'dark_aqua',
+    '4': 'dark_red',
+    '5': 'dark_purple',
+    '6': 'gold',
+    '7': 'gray',
+    '8': 'dark_gray',
+    '9': 'blue',
+    'a': 'green',
+    'b': 'aqua',
+    'c': 'red',
+    'd': 'light_purple',
+    'e': 'yellow',
+    'f': 'white'
+}
+
+
+def int_to_roman(number):
+    result = []
+    for value, symbol in ROMAN_VALUES:
+        quotient, remainder = divmod(number, value)
+        result.append(symbol * quotient)
+        if remainder == 0:
+            break
+    return ''.join(result)
 
 
 class SkyblockItem:
     def __init__(self, **kwargs):
-        self.enchantments = self.reforges = self.stats = {}
+        self.enchantments = {}
+        self.reforges = {}
+        self.stats = {}
 
         for kw in kwargs:
             if kw == 'skyblock_item':
@@ -99,6 +149,26 @@ class SkyblockItem:
                     stats[stat] = value, percent
 
         return stats
+    
+    def format_colors(self, string, default_color='gray'):
+        result = []
+        format_regex = re.compile(r'(:)([0-9a-f])\{(.*?)\}')
+        splitted = format_regex.split(string)
+        is_seq = False
+        text = {'italic': False}
+        for s in splitted:
+            if s == ':' and not is_seq:
+                is_seq = True
+            elif re.compile(r'[0-9a-f]').fullmatch(s) and is_seq:
+                text['color'] = COLOR_VALUES[s]
+            else:
+                text['text'] = s
+                if not is_seq:
+                    text['color'] = default_color
+                result.append(text)
+                text.clear()
+                is_seq = False
+        return json.dumps(result)
 
     def give_cmd(self, args):
         result = '/give '
@@ -107,27 +177,63 @@ class SkyblockItem:
         result += f'{target} {self.item}'
         enchantments = []
         display_enchantments = []
-        name = {'text': ''}
+        name = {'text': self.name, 'color': self.rarity[1], 'italic': False}
         lore = []
 
         for stat in self.final_stats:
             quantifier = str(self.final_stats[stat][0])
             if self.final_stats[stat][1]:
                 quantifier += '%'
-            line = [{'text': f'{stat} ', 'color': 'gray'},
-                    {'text': quantifier, 'color': 'red'}]
+            line = [{'text': f'{stat} ', 'color': 'gray', 'italic': False},
+                    {'text': quantifier, 'color': 'red', 'italic': False}]
             for reforge, ref_stats in self.reforges.items():
                 if stat in ref_stats:
                     line.append({
                         'text': f' ({reforge} {ref_stats[stat]})',
-                        'color': 'dark_gray'
+                        'color': 'dark_gray',
+                        'italic': False
                     })
-            lore.append(line)
+            lore.append(json.dumps(line))
+        lore.append('')
 
         for ench, (desc, lvl, vanilla) in self.enchantments.items():
             if vanilla is not None:
                 enchantments.append({'id': vanilla, 'lvl': lvl})
-            display_enchantments.append((ench, desc))
+            display_enchantments.append((ench, lvl, desc))
+
+        if len(display_enchantments) > 4:
+            line = ''
+            for i, (ench, lvl, _) in enumerate(display_enchantments):
+                line += f'{ench} {int_to_roman(lvl)}, '
+                if len(line) > 42 or i == len(display_enchantments) - 1:
+                    lore.append(json.dumps({
+                        'text': line,
+                        'color': 'blue',
+                        'italic': False
+                    }))
+                    line = ''
+            lore.append('')
+        else:
+            for ench, lvl, desc in display_enchantments:
+                lore.append(json.dumps({
+                    'text': f'{ench} {int_to_roman(lvl)}',
+                    'color': 'blue',
+                    'italic': False
+                }))
+                lore.append(self.format_colors(desc))
+                lore.append('')
+        
+        lore.append(json.dumps({
+            'text': f'{self.rarity[0]} {self.type}',
+            'color': COLOR_VALUES[self.rarity[1]],
+            'bold': True,
+            'italic': False
+        }))
+
+        result += f'{{display:{{Name:{json.dumps(name)!r},'
+        result += f'Lore:{json.dumps(lore)}}},Unbreakable:true,'
+        result += f'Enchantments:{json.dumps(enchantments)}}}'
+        return result
 
 
 def decode_skyblock(dct):
@@ -142,7 +248,11 @@ def parse_args():
         'filename', help='the name of the JSON file that contains the item'
     )
     parser.add_argument(
-        '--target', '-t', help='the target of the give command'
+        '--target', '-t', help='the target of the give command (default @p)'
+    )
+    parser.add_argument(
+        '--output', '-o', help='the output stream of the '
+                               'program (default stdout)'
     )
     args = parser.parse_args()
     return args
@@ -156,11 +266,19 @@ def get_data(args):
             return None
 
 
+def send_to_output(args, cmd):
+    output = args.output
+    if output is None:
+        print(cmd)
+
+
 def main():
     args = parse_args()
     item = get_data(args)
     if not isinstance(item, SkyblockItem):
         raise TypeError('Not a Skyblock item formatted file')
+    cmd = item.give_cmd(args)
+    send_to_output(args, cmd)
 
 
 if __name__ == '__main__':
